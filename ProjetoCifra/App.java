2ccef42357a0174302c5f539002945ff5d726791
Emanuelly Plens <plensmanu777@gmail.com>
2026-05-11 15:06:07 -0400

    Atualiza mensagens de saída para incluir detalhes dos mistérios A2 A e A2 B

--- ProjetoCifra/App.java
+++ ProjetoCifra/App.java
@@ -4,7 +4,8 @@ import ProjetoCifra.Desafio.*;
 
 public class App {
   public static void main (String[] args) throws Exception {
-    System.out.println("=================== Solução do Mistério ===================");
+    System.out.println("=================== Solução do Mistério A2 B ===================");
+    
     String local = Decifrador.decifrarCesar("JSYWFIF", 5);
     System.out.println("Local do crime: " + local);
 
@@ -14,5 +15,20 @@ public class App {
     String objeto = Decifrador.decifrarRC4("C8 28 BB FE 97 9D 8B E7 F0 C4 8E 9C", local);
     System.out.println("Objeto do crime: " + objeto);
 
+    
+    System.out.println("==================== Solução do Mistério A2 A ====================");
+
+
+    String protocolo = Decifrador.decifrarVigenere("T_X_N_S_R_L", "MXLINUX");
+    System.out.println("O Protocolo de Acesso: " + protocolo);
+
+
+    String arquivo = Decifrador.decifrarRC4("B1 F4 C6 6C F6 7D 4F", protocolo.replace("_", ""));
+    System.out.println("O Arquivo de Configuração: " + arquivo);
+
+
+    String localizacao = Decifrador.decifrarCesar("VIMXSV", 4);
+    System.out.println("A Localização Final: " + localizacao);
   }
 }
+
