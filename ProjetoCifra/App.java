efb52716bd2c0a02b19cfd67ce1d0267563a76d9
Emanuelly Plens <plensmanu777@gmail.com>
2026-05-25 20:01:59 -0400

    Ajusta formatação e organiza saídas de console para os mistérios A2 A e A2 C

--- ProjetoCifra/App.java
+++ ProjetoCifra/App.java
@@ -3,9 +3,9 @@ package ProjetoCifra;
 import ProjetoCifra.Desafio.*;
 
 public class App {
-  public static void main (String[] args) throws Exception {
+  public static void main(String[] args) throws Exception {
     System.out.println("=================== Solução do Mistério A2 B ===================");
-    
+
     String local = Decifrador.decifrarCesar("JSYWFIF", 5);
     System.out.println("Local do crime: " + local);
 
@@ -15,20 +15,27 @@ public class App {
     String objeto = Decifrador.decifrarRC4("C8 28 BB FE 97 9D 8B E7 F0 C4 8E 9C", local);
     System.out.println("Objeto do crime: " + objeto);
 
-    
-    System.out.println("==================== Solução do Mistério A2 A ====================");
-
+    System.out.println("\n==================== Solução do Mistério A2 A ====================");
 
     String protocolo = Decifrador.decifrarVigenere("T_X_N_S_R_L", "MXLINUX");
     System.out.println("O Protocolo de Acesso: " + protocolo);
 
-
     String arquivo = Decifrador.decifrarRC4("B1 F4 C6 6C F6 7D 4F", protocolo.replace("_", ""));
     System.out.println("O Arquivo de Configuração: " + arquivo);
 
-
     String localizacao = Decifrador.decifrarCesar("VIMXSV", 4);
     System.out.println("A Localização Final: " + localizacao);
+
+    System.out.println("\n==================== Solução do Mistério A2 C ====================");
+
+    String despertar = Decifrador.decifrarCesar("ZPNH WHYH H IPISPVALJH", 7);
+    System.out.println("O despertar: " + despertar);
+
+    String arquivoConfiguracao = Decifrador.decifrarVigenere("AZJTICIM RHCIN UC BCVUFZ VJ GSCQJUC XIOLHTVJ", "LIVRO");
+    System.out.println("O arquivo de configuração: " + arquivoConfiguracao);
+
+    String objetivo = Decifrador.decifrarRC4("2C D2 AC 57 EB 75 18 CD C0 1B 2C D5 01 72 05 C3 5B 50 3F 33 40 CF FB 7E A4 99 D8 BC 49 7F 44 45 C1 28 55", "SALDO");
+    System.out.println("Objetivo final: " + objetivo);
+
   }
 }
-
